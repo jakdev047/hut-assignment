083ff41fd0ea7652895acb64cbd68ae4cb8cc0fa
jakdev047 <jakfreelancer047@gmail.com>
2023-06-30 12:01:00 +0600

    cow module created

---
+++ src/modules/cow/cow.controller.ts
@@ -0,0 +1,78 @@
+import { Request, RequestHandler, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import responseFormat from "../../shared/responseFormat";
+import { ICow } from "./cow.interface";
+import {
+  createCowService,
+  deleteCowService,
+  getAllCowService,
+  getSingleCowService,
+  updateCowService,
+} from "./cow.service";
+import { pick } from "../../shared/pick";
+import { cowFilterableFields } from "./cow.constant";
+import { paginationFields } from "../../shared/paginationFields";
+
+export const createCow: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { ...cowData } = req.body;
+    const result = await createCowService(cowData);
+    responseFormat<ICow>(res, {
+      success: true,
+      statusCode: 200,
+      message: "Cow created successfully",
+      data: result,
+    });
+  }
+);
+
+export const getAllCows = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, cowFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
+  const result = await getAllCowService(filters, paginationOptions);
+
+  responseFormat<ICow[]>(res, {
+    statusCode: 200,
+    success: true,
+    message: "Cows retrieved successfully !",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+export const getSingleCow = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await getSingleCowService(id);
+
+  responseFormat<ICow>(res, {
+    statusCode: 200,
+    success: true,
+    message: "Cow retrieved successfully",
+    data: result,
+  });
+});
+
+export const updateCow = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updatedData = req.body;
+  const result = await updateCowService(id, updatedData);
+
+  responseFormat<ICow>(res, {
+    statusCode: 200,
+    success: true,
+    message: "Cow updated successfully",
+    data: result,
+  });
+});
+
+export const deleteCow = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await deleteCowService(id);
+
+  responseFormat<ICow>(res, {
+    statusCode: 200,
+    success: true,
+    message: "Cow deleted successfully",
+    data: result,
+  });
+});
---
+++ src/modules/cow/cow.interface.ts
@@ -0,0 +1,33 @@
+import { Model, Types } from "mongoose";
+
+export type ILocation =
+  | "Dhaka"
+  | "Chattogram"
+  | "Barishal"
+  | "Rajshahi"
+  | "Sylhet"
+  | "Comilla"
+  | "Rangpur"
+  | "Mymensingh";
+export type ICategory = "Dairy" | "Beef" | "Dual Purpose";
+
+export type ICow = {
+  name: string;
+  age: number;
+  price: number;
+  location: ILocation;
+  weight: number;
+  breed: string;
+  label: "for sale" | "sold out";
+  category: ICategory;
+  seller?: Types.ObjectId;
+};
+
+export type CowModel = Model<ICow, Record<string, unknown>>;
+
+export type ICowFilters = {
+  searchTerm?: string;
+  location?: string;
+  minPrice?: number;
+  maxPrice?: number;
+};
