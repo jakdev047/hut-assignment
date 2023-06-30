ec4c1788725f10a7be194865764abe8ffd869c16
jakdev047 <jakfreelancer047@gmail.com>
2023-06-30 12:15:53 +0600

    app file modified

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { UserRoutes } from "./modules/user/user.routes";
+import { CowRoutes } from "./modules/cow/cow.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
+import { orderRoutes } from "./modules/order/order.routes";
 const app: Application = express();
 
 app.use(cors());
@@ -8,9 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.use("/api/v1", UserRoutes);
+app.use("/api/v1", CowRoutes);
+app.use("/api/v1", authRoutes);
+app.use("/api/v1", orderRoutes);
 
 // error handler
 app.use(globalErrorHandler);
